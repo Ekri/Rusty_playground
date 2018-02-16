299200bc8e8075f8320c515b82944536fabf60ed
Bartosz Maciega <b.maciega@futuremind.com>
2018-02-16 11:42:10 +0100

    simple guessing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main(){
-    println!("Hello, world")
+use std::io::stdin;
+
+fn main() {
+    println!("Hello, world");
+    guessing_game()
+}
+
+fn guessing_game() {
+    println!("Guess the number!");
+
+    println!("Please input your guess.");
+
+    let mut guess = String::new();
+
+    stdin().read_line(&mut guess)
+        .expect("Failed to read line");
+    println!("You guessed: {}", guess)
 }
