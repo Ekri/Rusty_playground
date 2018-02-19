d61cada9d55b679b6992709bd8c2ea673720b25b
Bartosz Maciej <bartoszmaciej@Bartoszs-MacBook-Pro.local>
2018-02-19 20:17:03 +0100

    playing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main(){
-    println!("Hello, world")
+fn main() {
+    println!("Hello, world");
+    arrays()
+}
+
+fn data_types() -> String {
+    let different = 13.45;
+    return different.to_string();
+}
+
+fn tuples() {
+    let tupl: (i16, i16, &str) = (11, 5, "dup");
+    let first = tupl.2;
+    println!("tupl {}", first);
+}
+
+fn arrays() {
+    let arr = ["du", "de", "re", "me"];
+    let os = arr[2];
+    println!("arr {}",os)
 }
