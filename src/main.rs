3a1aaeb9a8f95396724a7133a053cee35a3df473
Bartosz Maciej <bartoszmaciej@Bartoszs-MacBook-Pro.local>
2018-02-23 18:36:20 +0100

    structs basic example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
-fn main(){
-    println!("Hello, world")
+fn main() {
+    println!("Hello, world");
+    let user1 = build_user(String::from("user"), String::from("user"));
+
+    let user2 = User {
+        email: String::from("mailo"),
+        ..user1
+    };
 }
+
+struct User {
+    username: String,
+    email: String,
+    sign_in_count: u64,
+    active: bool,
+}
+
+fn build_user(email: String, username: String) -> User {
+    User {
+        username,
+        email,
+        sign_in_count: 1,
+        active: true,
+    }
+}
+
+struct TupleStruct(i16, i16, i16);
