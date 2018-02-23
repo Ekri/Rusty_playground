48b3aa4e10f1763170e33d666e78bc3cfca83ed3
Bartosz Maciej <bartoszmaciej@Bartoszs-MacBook-Pro.local>
2018-02-23 16:49:55 +0100

    cloning, passing reference as an argument

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,26 @@
 fn main() {
     println!("Hello, world");
-    let x = String::from("dupa");
-    let x2 = x;
-
-    println!("In scope {}",x2)
+    reference();
 }
 
-fn cloning(){
+fn scoping() {
+    let x = String::from("dupa");
+    let x2 = x;
+    println!("In scope {}", x2);
+}
 
+fn cloning() {
+    let x = String::from("dupa");
+    let x2 = x.clone();
+    println!("Scope after clone {}", x2)
+}
+
+fn reference() {
+    let s = String::from("helllllooooo");
+    let length = calculate_length(&s);
+    println!("String {}, and its lenght {}", s, length)
+}
+
+fn calculate_length(s: &String) -> usize {
+    s.len()
 }
