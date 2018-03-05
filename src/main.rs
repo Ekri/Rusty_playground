17a2da9bf83556f8ef69a957880c3ad3e6c9cfdf
Bartosz Maciej <bartoszmaciej@Bartoszs-MacBook-Pro.local>
2018-03-05 18:20:09 +0100

    writing example tests

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main(){
+fn main() {
     println!("Hello, world")
+}
+
+pub fn add_two(a: i32) -> i32 {
+    a + 2
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+
+    #[test]
+    fn it_add_two() {
+        assert_eq!(4, add_two(2))
+    }
 }
