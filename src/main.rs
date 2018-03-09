48dcc61802fc6f1a64527012b02a1601f79cb405
Bartosz Maciej <bartoszmaciej@Bartoszs-MacBook-Pro.local>
2018-03-09 18:29:44 +0100

    further testing

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,22 @@ pub fn add_two(a: i32) -> i32 {
     a + 2
 }
 
+pub struct Guess {
+    value: u32
+}
+
+impl Guess {
+    pub fn new(value: u32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Guess value must be between 1 and 100, got {}.", value);
+        }
+
+        Guess {
+            value
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -18,5 +34,11 @@ mod tests {
     #[test]
     fn it_add_two() {
         assert_eq!(4, add_two(2))
+    }
+
+    #[test]
+    #[should_panic(expected ="Guess value must be between 1 and 100")]
+    fn greater_than_100() {
+        Guess::new(200);
     }
 }
