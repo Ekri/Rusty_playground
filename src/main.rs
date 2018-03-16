7cf9b3dcec4df632ccc218743a903c70fb03ab65
Bartosz Maciej <bartoszmaciej@Bartoszs-MacBook-Pro.local>
2018-03-16 18:55:14 +0100

    refactoring to use main - lib pattern

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,22 @@
+extern crate rusty;
+
 use std::env;
-use std::fs::File;
-use std::io::prelude::*;
+use std::process;
+use rusty::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let (query, filename) = parse_config(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments {}", err);
+        process::exit(1);
+    });
 
-    println!("Searching for {}", query);
-    println!("In file {}", filename);
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
 
-    let mut f = File::open(filename).expect("File not found");
-
-    let mut content = String::new();
-
-    f.read_to_string(&mut content).expect("something went wrong reading the file");
-
-    println!("text {}", content);
-}
-
-
-fn parse_config(args: &[String]) -> (&str, &str) {
-    let query = &args[1];
-    let filename = &args[2];
-    (query, filename)
+    if let Err(e) = rusty::run(config) {
+        println!("Application error {}", e);
+        process::exit(1);
+    };
 }
