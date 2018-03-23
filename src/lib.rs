3307c1adc308688d144679e9ed34fd2c79e46d15
Bartosz Maciej <bartoszmaciej@Bartoszs-MacBook-Pro.local>
2018-03-23 06:29:41 +0100

    iterators example, usage and implementation

---
+++ src/lib.rs
@@ -0,0 +1,54 @@
+#[derive(PartialEq, Debug)]
+struct Shoe {
+    size: u8,
+    style: String,
+}
+
+fn shoes_in_my_size(shoes: Vec<Shoe>, size: u8) -> Vec<Shoe> {
+    shoes.into_iter()
+        .filter(|s| s.size == size)
+        .collect()
+}
+
+#[test]
+fn filters_by_size() {
+    let shoes = vec![
+        Shoe { size: 10, style: String::from("sneaker") },
+        Shoe { size: 13, style: String::from("sandal") },
+        Shoe { size: 10, style: String::from("boot") },
+    ];
+
+    let in_my_size = shoes_in_my_size(shoes, 10);
+
+    assert_eq!(
+        in_my_size,
+        vec![
+            Shoe { size: 10, style: String::from("sneaker") },
+            Shoe { size: 10, style: String::from("boot") },
+        ]
+    );
+}
+
+struct Counter {
+    count: u32
+}
+
+impl Counter {
+    fn new() -> Counter {
+        Counter { count: 0 }
+    }
+}
+
+impl Iterator for Counter {
+    type Item = u32;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.count += 1;
+
+        if self.count < 6 {
+            Some(self.count)
+        } else {
+            None
+        }
+    }
+}
