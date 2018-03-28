b3792e4d63f0986ee90d17ef2c22df02b1aad6a0
Bartosz Maciega <b.maciega@futuremind.com>
2018-03-28 14:56:25 +0200

    multiple senders

--- src/main.rs
+++ src/main.rs
@@ -36,6 +36,23 @@ fn moving() {
 
 fn channels() {
     let (tx, rx) = mpsc::channel();
+
+    let tx1 = mpsc::Sender::clone(&tx);
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+
+        for val in vals {
+            tx1.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
     thread::spawn(move || {
         let vals = vec![
             String::from("hi"),
