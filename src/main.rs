d207ed36d64f7cf3dff13c19c93ad8a9dacb5351
Bartosz Maciej <bartoszmaciej@Bartoszs-MacBook-Pro.local>
2018-03-29 06:28:11 +0200

    inheritance first look

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+extern crate rusty;
+
+use rusty::Draw;
+
 fn main(){
     println!("Hello, world")
+}
+
+struct SelectBox {
+    width: u32,
+    height: u32,
+    options: Vec<String>,
+}
+
+impl Draw for SelectBox {
+    fn draw(&self) {
+        // Code to actually draw a select box
+    }
 }
