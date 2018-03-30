f03f9b41d62b2e4a70e403ae93b3b2eaa786caeb
Bartosz Maciej <bartoszmaciej@Bartoszs-MacBook-Pro.local>
2018-03-30 06:29:32 +0200

    matching

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
 fn main(){
     println!("Hello, world")
 }
+
+fn simple_match(){
+    let x = 1;
+
+    match x {
+        1 => println!("one"),
+        2 => println!("two"),
+        3 => println!("three"),
+        _ => println!("anything"),
+    }
+}
+
+fn match_with_named(){
+    let x = Some(5);
+    let y = 10;
+
+    match x {
+        Some(50) => println!("Got 50"),
+        Some(y) => println!("Matched, y = {:?}", y),
+        _ => println!("Default case, x = {:?}", x),
+    }
+
+    println!("at the end: x = {:?}, y = {:?}", x, y);
+}
