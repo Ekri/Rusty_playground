57a41ec0fdd0c03a8ff8944bf7586bd479a27a27
Bartosz Maciega <bartoszmaciej@Bartoszs-MacBook-Pro.local>
2018-05-18 06:04:18 +0200

    slices

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 fn main() {
     println!("Hello, world");
-    function_reference()
+    borrowing();
 }
 
 fn sums() {
@@ -36,3 +36,36 @@ fn function_reference() {
 fn takes_ownership(some_string: String) { println!("{}", some_string); }
 
 fn makes_copy(some_integer: i32) { println!("{}", some_integer); }
+
+fn borrowing() {
+    let mut s1 = String::from("Dupas");
+
+    let len = calculate_length(&s1);
+
+    change(&mut s1);
+
+    println!("len is {}", len);
+    println!("modified {}", s1);
+
+    let r2 = &s1;
+    let r1 = &s1;
+}
+
+fn calculate_length(s: &String) -> usize {
+    s.len()
+}
+
+fn change(s: &mut String) {
+    s.push_str(", ojaaa!")
+}
+
+fn find_space(s: &str) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+    return &s[..];
+}
