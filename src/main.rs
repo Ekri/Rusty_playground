aaa1c7fdd294406f0c7706578214afffce4a3d56
Bartosz Maciega <bartoszmaciej@Bartoszs-MacBook-Pro.local>
2018-05-28 09:47:40 +0200

    playing a little bit with vectors

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
-fn main(){
-    println!("Hello, world")
+fn main() {
+    println!("Hello, world");
+    iterate();
+}
+
+
+fn vectors() {
+    let v: Vec<i32> = Vec::new();
+    let mut y = vec![1, 2, 3];
+    y.push(4);
+    y.push(5);
+}
+
+fn iterate() {
+    let mut v = vec![100, 12, 18, 3, 22];
+    for i in &mut v {
+        *i = 50;
+    }
+
+    for i in &v {
+        println!("{}", i);
+    }
+}
+
+enum SpreadSheet {
+    Int(i32),
+    Float(f64),
+    Text(String),
+}
+
+
+fn vector_with_enum() {
+    let row = vec![
+        SpreadSheet::Float(12.22),
+        SpreadSheet::Float(44.3),
+        SpreadSheet::Int(11),
+        SpreadSheet::Text(String::from("dupa"))
+    ];
 }
