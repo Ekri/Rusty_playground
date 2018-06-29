c543187de55db9a51f6489d94e602e2227bb72d9
Bartosz Maciega <bartoszmaciej@Bartoszs-MacBook-Pro.local>
2018-06-29 15:14:41 +0200

    iterative version of fibbonaci, check if can do it smarter

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
-fn main(){
-    println!("Hello, world")
+fn main() {
+    println!("Hello, world {}", fibonacci(4))
 }
+
+fn calculate_fahrenheit(cels: f64) -> f64 {
+    cels * 1.8
+}
+
+fn fibonacci(nth: i64) -> i64 {
+    if nth <= 2 {
+        return 1;
+    }
+
+    let mut index: i64 = nth - 2;
+
+    let mut first: i64 = 1;
+    let mut second: i64 = 1;
+    let mut current_result: i64 = 0;
+
+    while index < nth {
+        current_result = first + second;
+        first = second;
+        second = current_result;
+
+        println!("Loop: {}  result {}", index, current_result);
+        index += 1;
+    }
+
+    return current_result;
+}
+
